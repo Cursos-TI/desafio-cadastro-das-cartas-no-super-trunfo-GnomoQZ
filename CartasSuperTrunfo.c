e05f430e922282ac44d339921982aaf9fa4fd8fd
GnomoQZ <202402914677@alunos.estacio.br>
2025-02-01 01:43:19 +0000

    foi utilizdo a função tyepdef struct

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,26 +11,38 @@ int main() {
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     printf("====== Desafio super trunfo ========= \n\n");
     printf("Cadrato das cartas:\n");
-    int populacao,ponto_turistuco,pib;
-    char nome_cidade[30],codigo_cidade[5];
+    // Definição da struct
+    typedef struct{
+    int populacao;
+    int ponto_turistuco;
+    int pib;
+    char nome_cidade[30];
+    char codigo_cidade[5];
     float area;
+    } carta_A;
+    // Criando um array de 3 alunos
+    carta_A ficha[4];
+    // Loop para preencher os dados das cartas
+    for (int i = 0; i < 4; i++){
+    printf("\nDigite o codigo da cidade A%02d:\n", i + 1); // Exibe A01, A02, A03, A04.
     printf("Digite codigo da cidade: ");
-    scanf("%s", &codigo_cidade);
+    scanf("%s", &ficha[i].codigo_cidade);
 
     printf("Digite o nome da cidade: ");
-    scanf(" %s", &nome_cidade);
+    scanf(" %s", &ficha[i].nome_cidade);
 
     printf("Digite o numero da populacao da cidade: ");
-    scanf(" %d", &populacao);
+    scanf(" %d", &ficha[i].populacao);
 
     printf("Digite o numero de pontos turisticos: ");
-    scanf(" %d", &ponto_turistuco);
+    scanf(" %d", &ficha[i].ponto_turistuco);
 
     printf("Digite o PIB da cidade: ");
-    scanf(" %d", &pib);
+    scanf(" %d", &ficha[i].pib);
 
     printf("Digite a Area da cidade: ");
-    scanf("%F", &area);
+    scanf("%F", &ficha[i].area);
+    }
     printf("\n");
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
@@ -38,12 +50,16 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-    printf("==== Informçôes das cartas e atributos cadrastados ======= \n");
-    printf("Codigo da cicade: %s\n", codigo_cidade);
-    printf("Nome da cidade: %s\n", nome_cidade);
-    printf("Numero da população: %d\n", populacao);
-    printf("Numero de pontos turisticos: %d\n", ponto_turistuco);
-    printf("PIB da cidade: %d\n", pib);
-    printf("Area demografica da cidade: %f km²", area);
+    //Exibir os dados dos alunos.
+    printf("\n\n==== Informçôes das cartas e atributos cadrastados =======\n\n");
+    for (int i = 0; i < 4; i++) {
+    printf("Carta A%02d\n", i + 1);  // Exibe A01, A02, A03
+    printf("Codigo da cicade: %s\n", ficha[i].codigo_cidade);
+    printf("Nome da cidade: %s\n", ficha[i].nome_cidade);
+    printf("Numero da população: %d\n", ficha[i].populacao);
+    printf("Numero de pontos turisticos: %d\n", ficha[i].ponto_turistuco);
+    printf("PIB da cidade: %d\n", ficha[i].pib);
+    printf("Area demografica da cidade: %f km²\n\n",ficha[i].area);
+    }
     return 0;
 }
